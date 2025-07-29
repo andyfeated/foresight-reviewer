c0c1f285c42f665f74ded03c4d6743f9db14315a
peterban2001 <peterban2001@gmail.com>
2025-07-30 06:10:47 +0800

    feat: initial commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+
+dotenv.config()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'Gitlab Service is running' })
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Gitlab Service is running on port ${PORT}`)
+})
