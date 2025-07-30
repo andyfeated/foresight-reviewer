73b9ea49a54ccf50333aa4fe4b5ca56312d90cfc
peterban2001 <peterban2001@gmail.com>
2025-07-31 07:59:05 +0800

    feat: use gitlab-service /pr endpoint for getting merge request data

--- src/services/reviewService.ts
+++ src/services/reviewService.ts
@@ -4,20 +4,32 @@ interface ReviewPullRequestPayload {
 }
 
 export class ReviewService {
+  private gitlabServiceBaseUrl: string;
+
+  constructor() {
+    this.gitlabServiceBaseUrl = process.env.GITLAB_SERVICE_URL as string;
+  }
+  
   public async reviewPullRequest(payload: ReviewPullRequestPayload) {
     try {
       const pullRequestUrl = payload.pullRequestUrl;
 
-      const pullRequestResponse = await fetch(pullRequestUrl)
+      const pullRequestResponse = await fetch(`${this.gitlabServiceBaseUrl}/pr`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }, 
+        body: JSON.stringify({ pullRequestUrl })
+      })
 
-      const responseStatus = pullRequestResponse.status
+      const pullRequestData = await pullRequestResponse.json()
 
-      console.log(responseStatus)
+      const pullRequestStatus = pullRequestData.status
 
-      if (responseStatus === 200) {
-        // public
-      } else if (responseStatus === 403) {
-        // private or doesnt exists
+     
+      console.log(pullRequestData.status)
+      if (pullRequestStatus === 200) {
+        console.log('public repo')
+      } else if (pullRequestStatus === 404) {
+        console.log('private repo or does not exist')
       }
       
       return { success: true }
