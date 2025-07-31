76cfb8be6301e867b49861168ce97339ebc61f91
peterban2001 <peterban2001@gmail.com>
2025-07-31 21:26:48 +0800

    fix: add web to docker

---
+++ src/components/base/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar: React.FC = () => {
+  return (
+    <header className="p-5 border-b border-gray-800">
+      test1
+    </header>
+  )
+}
+
+export default Navbar
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,12 @@ export default defineConfig({
       target: 'react',
       autoCodeSplitting: true,
     }),
+    tailwindcss(),
     react(),
     // ...,
   ],
+  server: {
+    host: true,
+    port: 4100
+  }
 })
